6fd44aae14fdf8710cfcf1fa654c015e7f28ae9f
CehStephen <41102405+cehstephen@users.noreply.github.com>
2026-04-01 05:59:36 +0100

    Bump version to 1.2.2

--- jetio/__init__.py
+++ jetio/__init__.py
@@ -19,7 +19,7 @@ re-exporting key components from various modules to provide a simple
 and unified public API for developers.
 """
 
-__version__ = "1.2.1"
+__version__ = "1.2.2"
 
 # --- External Dependencies ---
 from pydantic import ValidationError
